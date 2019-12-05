e421d2ca2ec9018cc3479d3b76de67762a71e5d6
makotosd <mouri.makoto@gmail.com>
2019-12-05 22:56:35 +0900

    IP -> service name

--- TemperatureService.sh
+++ TemperatureService.sh
@@ -1,1 +1,2 @@
-docker run --rm --privileged -p 8080:8080 tmc05/temperature_svc
+#docker run -d --rm --privileged -p 8080:8080 --network sensor-network --name sensor tmc05/temperature_svc
+docker run -d --rm --privileged --network sensor-network --name sensor tmc05/temperature_svc
